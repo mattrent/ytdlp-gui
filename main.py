7d06c7b1d6e1b8ab4fc4d34b511bb74f9072355f
Matteo Trentin <matteo.trentin2@unibo.it>
2025-03-30 04:00:59 +0200

    add behaviour + requirements

--- main.py
+++ main.py
@@ -3,27 +3,55 @@
 
 # TODO: setup dependabot to auto-update and auto-release when a new version of yt_dlp comes out
 # TODO: auto-upload .exe release
+# TODO: download new version when opening app
 
 import os
-from kivy.app import App
+from threading import Thread
+import validators
 
-from kivy.uix.button import Button
-from kivy.uix.dropdown import DropDown
-from kivy.uix.textinput import TextInput
+from kivy.config import Config
+
+Config.set("graphics", "resizable", "0")
+Config.set("graphics", "height", "360")
+
+from kivymd.app import MDApp
 
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy.uix.dropdown import DropDown
+
+from kivymd.uix.button import MDIconButton
+from kivymd.uix.label import MDLabel
+from kivymd.uix.textfield import MDTextField
+from kivymd.uix.progressbar import MDProgressBar
 
 from plyer import filechooser
 
 from yt_dlp import YoutubeDL
 
 
-class App(App):
+class App(MDApp):
 
     def __init__(self):
         super(App, self).__init__()
         self.location = self.get_download_path()
+        self.title = "yt-dlp GUI"
+        self.formats = {
+            "mp3": {
+                "postprocessors": [
+                    {"key": "FFmpegExtractAudio", "preferredcodec": "mp3"}
+                ],
+                "outtmpl": {"default": "%(title)s.%(ext)s"},
+                "noprogress": True,
+                "quiet": True,
+            },
+            "mp4": {
+                "format": "mp4",
+                "outtmpl": {"default": "%(title)s.%(ext)s"},
+                "noprogress": True,
+                "quiet": True,
+            },
+        }
 
     def get_download_path(self):
         if os.name == "nt":
@@ -43,6 +71,46 @@ class App(App):
         path = filechooser.choose_dir()
         self.location = path
 
+    def download_video(self, url):
+        if validators.url(url):
+            self.url_input.error = False
+            self.url_input.disabled = True
+            self.download_button.disabled = True
+            self.options_button.disabled = True
+            self.location_button.disabled = True
+            thread = Thread(target=self.download_video_thread, args=(url,))
+            thread.start()
+        else:
+            self.url_input.error = True
+
+    def download_video_thread(self, url):
+        try:
+            ytdl_opts = self.formats[self.options_button.text]
+            ytdl_opts["paths"] = {"home": self.location}
+            ytdl_opts["progress_hooks"] = [self.progress_hook]
+            with YoutubeDL(ytdl_opts) as ytdl:
+                self.video_name_label.text = "Fetching..."
+                ytdl.download([url])
+        except:
+            self.video_name_label.text = "Error :("
+
+        self.url_input.disabled = False
+        self.download_button.disabled = False
+        self.options_button.disabled = False
+        self.location_button.disabled = False
+
+    def progress_hook(self, d):
+        self.video_name_label.text = d["info_dict"]["title"]
+        self.status_label.text = d["status"]
+        self.percentage_label.text = f'{d["_percent"]:.1f}%'
+        speed = d["speed"]
+        if speed:
+            speed /= 1024 * 1024
+        else:
+            speed = 0
+        self.speed_label.text = f"{speed:.1f}MiB/s"
+        self.progress_bar.value = int(float(d["_percent"]))
+
     def build(self):
         root_layout = BoxLayout(orientation="vertical", spacing=50, padding=30)
 
@@ -50,8 +118,23 @@ class App(App):
         url_download_layout = BoxLayout(
             orientation="horizontal", spacing=20, size_hint_y=0.2
         )
-        url_input = TextInput(size_hint_x=0.8)
-        download_button = Button(size_hint_x=0.2)
+        self.url_input = MDTextField(
+            size_hint_x=0.9,
+            hint_text="URL",
+            mode="rectangle",
+            multiline=False,
+            helper_text="Enter a valid URL",
+        )
+        self.download_button = MDIconButton(size_hint_x=0.1, icon="download")
+
+        ### setup download button and URL input
+        self.download_button.bind(
+            on_release=lambda _: self.download_video(self.url_input.text)
+        )
+
+        self.video_name_label = MDLabel(
+            text="", size_hint_x=1, size_hint_y=0.05, halign="center"
+        )
 
         # setup location/options sublayout
         location_options_layout = BoxLayout(
@@ -59,28 +142,45 @@ class App(App):
         )
 
         ### setup location button
-        location_button = Button(size_hint_x=0.5, text=self.location)
-        location_button.bind(on_release=lambda btn: self.pick_download_folder(btn))
+        self.location_button = Button(size_hint_x=0.5, text=self.location)
+        self.location_button.bind(on_release=lambda btn: self.pick_download_folder(btn))
         ### setup dropdown
-        options_dropdown = DropDown()
-        for format in [".mp3", ".webm", ".mp4", ".mkv"]:
-            btn = Button(text=format, size_hint_y=None, height=24)
-            btn.bind(on_release=lambda btn: options_dropdown.select(btn.text))
-            options_dropdown.add_widget(btn)
-        options_dropdown.bind(
-            on_select=lambda instance, x: setattr(options_button, "text", x)
+        self.options_dropdown = DropDown()
+        for format in self.formats:
+            btn = Button(text=format, size_hint_y=None, height=32, font_size=16)
+            btn.bind(on_release=lambda btn: self.options_dropdown.select(btn.text))
+            self.options_dropdown.add_widget(btn)
+        self.options_dropdown.bind(
+            on_select=lambda _, x: setattr(self.options_button, "text", x)
         )
-        options_button = Button(text="Select format...", size_hint_x=0.5)
-        options_button.bind(on_release=options_dropdown.open)
+        self.options_button = Button(text="mp3", size_hint_x=0.5)
+        self.options_button.bind(on_release=self.options_dropdown.open)
+
+        # progress labels layout
+        progress_labels_layout = BoxLayout(
+            orientation="horizontal", spacing=20, size_hint_y=0.2
+        )
+
+        self.status_label = MDLabel(text="", halign="center")
+        self.speed_label = MDLabel(text="", halign="center")
+        self.percentage_label = MDLabel(text="", halign="center")
+
+        self.progress_bar = MDProgressBar(max=100, value=0, size_hint_y=0.4)
 
         # add widgets to layouts
-        url_download_layout.add_widget(url_input)
-        url_download_layout.add_widget(download_button)
-        location_options_layout.add_widget(location_button)
-        location_options_layout.add_widget(options_button)
+        url_download_layout.add_widget(self.url_input)
+        url_download_layout.add_widget(self.download_button)
+        location_options_layout.add_widget(self.location_button)
+        location_options_layout.add_widget(self.options_button)
+        progress_labels_layout.add_widget(self.status_label)
+        progress_labels_layout.add_widget(self.speed_label)
+        progress_labels_layout.add_widget(self.percentage_label)
 
         root_layout.add_widget(url_download_layout)
+        root_layout.add_widget(self.video_name_label)
         root_layout.add_widget(location_options_layout)
+        root_layout.add_widget(progress_labels_layout)
+        root_layout.add_widget(self.progress_bar)
 
         return root_layout
 
