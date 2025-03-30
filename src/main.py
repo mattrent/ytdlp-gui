c9beb3258270b479f3a85eb61d79639a6dd9e97b
Matteo Trentin <matteo.trentin2@unibo.it>
2025-03-30 22:25:12 +0200

    migrate to eel

--- src/main.py
+++ src/main.py
@@ -4,191 +4,131 @@
 # TODO: setup dependabot to auto-update and auto-release when a new version of yt_dlp comes out
 # TODO: auto-upload .exe release
 # TODO: download new version when opening app
-# TODO: migrate to customtkinter
-# https://github.com/TomSchimansky/CustomTkinter/discussions/423#discussioncomment-3549223
-# https://github.com/TomSchimansky/CustomTkinter/issues/631
 
 import os
 from threading import Thread
 import validators
 
-from kivy.config import Config
-
-Config.set("graphics", "resizable", "0")
-Config.set("graphics", "height", "360")
-
-from kivymd.app import MDApp
-
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.uix.dropdown import DropDown
-
-from kivymd.uix.button import MDIconButton
-from kivymd.uix.label import MDLabel
-from kivymd.uix.textfield import MDTextField
-from kivymd.uix.progressbar import MDProgressBar
-from kivymd.icon_definitions import md_icons
+import eel
 
 from plyer import filechooser
-
 from yt_dlp import YoutubeDL
 
 
-class App(MDApp):
+video_label = ""
+video_format = "mp3"
+url = ""
 
-    def __init__(self):
-        super(App, self).__init__()
-        self.location = self.get_download_path()
-        self.title = "yt-dlp GUI"
-        self.formats = {
-            "mp3": {
-                "postprocessors": [
-                    {"key": "FFmpegExtractAudio", "preferredcodec": "mp3"}
-                ],
-                "outtmpl": {"default": "%(title)s.%(ext)s"},
-                "noprogress": True,
-                "quiet": True,
-            },
-            "mp4": {
-                "format": "mp4",
-                "outtmpl": {"default": "%(title)s.%(ext)s"},
-                "noprogress": True,
-                "quiet": True,
-            },
-        }
+formats = {
+    "mp3": {
+        "postprocessors": [{"key": "FFmpegExtractAudio", "preferredcodec": "mp3"}],
+        "outtmpl": {"default": "%(title)s.%(ext)s"},
+        "noprogress": True,
+        "quiet": True,
+    },
+    "mp4": {
+        "format": "mp4",
+        "outtmpl": {"default": "%(title)s.%(ext)s"},
+        "noprogress": True,
+        "quiet": True,
+    },
+}
 
-    def get_download_path(self):
-        if os.name == "nt":
-            import winreg
 
-            sub_key = (
-                r"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders"
-            )
-            downloads_guid = "{374DE290-123F-4565-9164-39C4925E467B}"
-            with winreg.OpenKey(winreg.HKEY_CURRENT_USER, sub_key) as key:
-                location = winreg.QueryValueEx(key, downloads_guid)[0]
-            return location
-        else:
-            return os.path.join(os.path.expanduser("~"), "Downloads")
+@eel.expose
+def get_default_download_path():
+    if os.name == "nt":
+        import winreg
 
-    def pick_download_folder(self, _btn):
-        path = filechooser.choose_dir(multiple=False)
-        self.location = path[0]
-        self.location_button.text = path[0]
+        sub_key = r"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders"
+        downloads_guid = "{374DE290-123F-4565-9164-39C4925E467B}"
+        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, sub_key) as key:
+            location = winreg.QueryValueEx(key, downloads_guid)[0]
+        return location
+    else:
+        return os.path.join(os.path.expanduser("~"), "Downloads")
 
-    def download_video(self, url):
-        if validators.url(url):
-            self.url_input.error = False
-            self.url_input.disabled = True
-            self.download_button.disabled = True
-            self.options_button.disabled = True
-            self.location_button.disabled = True
-            thread = Thread(target=self.download_video_thread, args=(url,))
-            thread.start()
-        else:
-            self.url_input.error = True
 
-    def download_video_thread(self, url):
-        try:
-            ytdl_opts = self.formats[self.options_button.text]
-            ytdl_opts["paths"] = {"home": self.location}
-            ytdl_opts["progress_hooks"] = [self.progress_hook]
-            with YoutubeDL(ytdl_opts) as ytdl:
-                self.video_name_label.text = "Fetching..."
-                ytdl.download([url])
-        except:
-            self.video_name_label.text = "Error :("
+@eel.expose
+def pick_download_folder():
+    global location
 
-        self.url_input.disabled = False
-        self.download_button.disabled = False
-        self.options_button.disabled = False
-        self.location_button.disabled = False
+    path = filechooser.choose_dir(multiple=False)
+    if path:
+        location = path
+        eel.update_download_path(path[0])()
 
-    def progress_hook(self, d):
-        self.video_name_label.text = d["info_dict"]["title"]
-        self.status_label.text = d["status"]
-        self.percentage_label.text = f'{d["_percent"]:.1f}%'
-        speed = d["speed"]
-        if speed:
-            speed /= 1024 * 1024
-        else:
-            speed = 0
-        self.speed_label.text = f"{speed:.1f}MiB/s"
-        self.progress_bar.value = int(float(d["_percent"]))
 
-    def build(self):
-        root_layout = BoxLayout(orientation="vertical", spacing=50, padding=30)
+@eel.expose
+def download_video(url, format, location):
+    if validators.url(url):
+        eel.set_url_input_error(False)
+        eel.set_controls_enabled(False)
+        eel.spawn(download_video_thread, url=url, format=format, location=location)
+    else:
+        eel.set_url_input_error(True)
 
-        # setup url/download sublayout
-        url_download_layout = BoxLayout(
-            orientation="horizontal", spacing=20, size_hint_y=0.2
-        )
-        self.url_input = MDTextField(
-            size_hint_x=0.9,
-            hint_text="URL",
-            mode="rectangle",
-            multiline=False,
-            helper_text="Enter a valid URL",
-        )
-        self.download_button = MDIconButton(size_hint_x=0.1, icon="download")
 
-        ### setup download button and URL input
-        self.download_button.bind(
-            on_release=lambda _: self.download_video(self.url_input.text)
-        )
+def download_video_thread(url, format, location):
+    global video_label
 
-        self.video_name_label = MDLabel(
-            text="", size_hint_x=1, size_hint_y=0.05, halign="center"
-        )
+    try:
+        ytdl_opts = formats[format]
+        ytdl_opts["paths"] = {"home": location}
+        ytdl_opts["progress_hooks"] = [progress_hook]
+        with YoutubeDL(ytdl_opts) as ytdl:
+            video_label = "Fetching..."
+            eel.update_video_label("Fetching...")
+            ytdl.download([url])
+    except Exception as e:
+        print(e)
+        video_label = "Error :("
+        eel.update_video_label("Error :(")
+    eel.set_controls_enabled(True)
 
-        # setup location/options sublayout
-        location_options_layout = BoxLayout(
-            orientation="horizontal", spacing=20, size_hint_y=0.2
-        )
 
-        ### setup location button
-        self.location_button = Button(size_hint_x=0.5, text=self.location)
-        self.location_button.bind(on_release=lambda btn: self.pick_download_folder(btn))
-        ### setup dropdown
-        self.options_dropdown = DropDown()
-        for format in self.formats:
-            btn = Button(text=format, size_hint_y=None, height=32, font_size=16)
-            btn.bind(on_release=lambda btn: self.options_dropdown.select(btn.text))
-            self.options_dropdown.add_widget(btn)
-        self.options_dropdown.bind(
-            on_select=lambda _, x: setattr(self.options_button, "text", x)
-        )
-        self.options_button = Button(text="mp3", size_hint_x=0.5)
-        self.options_button.bind(on_release=self.options_dropdown.open)
+def progress_hook(d):
+    global video_label
 
-        # progress labels layout
-        progress_labels_layout = BoxLayout(
-            orientation="horizontal", spacing=20, size_hint_y=0.2
-        )
+    video_label = d["info_dict"]["title"]
+    eel.update_video_label(d["info_dict"]["title"])
+    eel.update_status(d["status"], d["speed"], d["_percent"])
 
-        self.status_label = MDLabel(text="", halign="center")
-        self.speed_label = MDLabel(text="", halign="center")
-        self.percentage_label = MDLabel(text="", halign="center")
 
-        self.progress_bar = MDProgressBar(max=100, value=0, size_hint_y=0.4)
+@eel.expose
+def get_state():
+    state = {
+        "url": url,
+        "video_label": video_label,
+        "location": location,
+        "format": video_format,
+    }
+    return state
 
-        # add widgets to layouts
-        url_download_layout.add_widget(self.url_input)
-        url_download_layout.add_widget(self.download_button)
-        location_options_layout.add_widget(self.location_button)
-        location_options_layout.add_widget(self.options_button)
-        progress_labels_layout.add_widget(self.status_label)
-        progress_labels_layout.add_widget(self.speed_label)
-        progress_labels_layout.add_widget(self.percentage_label)
 
-        root_layout.add_widget(url_download_layout)
-        root_layout.add_widget(self.video_name_label)
-        root_layout.add_widget(location_options_layout)
-        root_layout.add_widget(progress_labels_layout)
-        root_layout.add_widget(self.progress_bar)
+@eel.expose
+def update_url(new_url):
+    global url
+    if new_url:
+        url = new_url
 
-        return root_layout
+
+@eel.expose
+def update_format(new_format):
+    global video_format
+    if new_format:
+        video_format = new_format
+
+
+def close(page, sockets_still_open):
+    os._exit(0)
 
 
 if __name__ == "__main__":
-    App().run()
+    global location
+    location = get_default_download_path()
+    eel.init("web")
+    eel.start("main.html", size=(540, 360), block=False, close_callback=close)
+    while True:
+        print("thread")
+        eel.sleep(1)
